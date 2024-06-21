efe08c7ae9f6f8602601660aee692ce1d7043e78
Moses Ojo <mosesadegoke.jo@gmail.com>
2024-06-21 16:31:11 +0100

    javascript project

--- src/main.js
+++ src/main.js
@@ -10,24 +10,24 @@ let shopItemsData = [
     }, 
     {
         id: "derder##",
-        name: "Casual Shirt",
-        price: 45,
+        name: "Office Shirt",
+        price: 100,
         desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
-        img: "images/img-1.jpg"    
+        img: "images/img-2.jpg"    
     }, 
     {
         id: "sweswe##",
-        name: "Casual Shirt",
-        price: 45,
+        name: "T Shirt",
+        price: 25,
         desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
-        img: "images/img-1.jpg"
+        img: "images/img-3.jpg"
     }, 
     {
         id: "aweawe##",
-        name: "Casual Shirt",
-        price: 45,
+        name: "Mens Suit",
+        price: 300,
         desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
-        img: "images/img-1.jpg"
+        img: "images/img-4.jpg"
     }];
 
 // Regular function vs ES6 arrow function 
@@ -36,23 +36,25 @@ let shopItemsData = [
 // The ES6 arrow function is more advanced than the regular function  
 
 let generateShop = () => {
-    return shop.innerHTML = `
-    <div class="item">
-        <img width="220" src="images/img-1.jpg" alt="">
-        <div class="details">
-            <h3>Casual Shirt</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
-            <div class="price-quantity">
-                <h2>$ 45</h2>
-                <div class="buttons">
-                    <i class="bi bi-dash-lg"></i>
-                    <div class="quantity">0</div>
-                    <i class="bi bi-plus-lg"></i>
+    return (shop.innerHTML = shopItemsData.map((x) => {
+        return `
+            <div class="item">
+                <img width="220" src="images/img-1.jpg" alt="">
+                <div class="details">
+                    <h3>Casual Shirt</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+                    <div class="price-quantity">
+                        <h2>$ 45</h2>
+                        <div class="buttons">
+                            <i class="bi bi-dash-lg"></i>
+                            <div class="quantity">0</div>
+                            <i class="bi bi-plus-lg"></i>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-    `;
+            `
+    }));
 };
 
 generateShop();
