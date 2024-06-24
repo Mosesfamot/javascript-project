712896d449756f7e6ad3e7090da2dd6d729d5a53
Moses Ojo <mosesadegoke.jo@gmail.com>
2024-06-24 15:22:15 +0100

    javascript project

--- src/main.js
+++ src/main.js
@@ -2,28 +2,28 @@ let shop = document.getElementById("shop");
 
 let shopItemsData = [
   {
-    id: "jfhgbvnscs",
+    id: "fffp1",
     name: "Casual Shirt",
     price: 45,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "images/img-1.jpg",
   },
   {
-    id: "ioytrhndcv",
+    id: "fffp2",
     name: "Office Shirt",
     price: 100,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "images/img-2.jpg",
   },
   {
-    id: "wuefbncxbsn",
+    id: "fffp3",
     name: "T Shirt",
     price: 25,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "images/img-3.jpg",
   },
   {
-    id: "thyfhcbcv",
+    id: "fffp4",
     name: "Mens Suit",
     price: 300,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
