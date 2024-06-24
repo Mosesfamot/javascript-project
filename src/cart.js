504ebf0593f8196f1505e005832175940d447914
Moses Ojo <mosesadegoke.jo@gmail.com>
2024-06-24 16:37:14 +0100

    javascript project

--- src/cart.js
+++ src/cart.js
@@ -12,15 +12,15 @@ calculation();
 
 let generateCartItems = () => {
     if(basket.length !== 0) {
-
-    }
-    else {
-        shoppingCart.innerHTML = '';
-        label.innerHTML = '
+    } else {
+        shoppingCart.innerHTML = ``;
+        label.innerHTML = `
         <h2>Cart is Empty</h2>
         <a href="index.html">
             <button class="HomeBtn">Back to home</button>
         </a>
-        ';
+        `;
     }
-}
+};
+
+generateCartItems();
